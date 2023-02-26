fa6569d0f326e3301d2c5136b87dd538b0e75229
albertkjoller <albert_k_jacobsen@hotmail.com>
2023-02-26 15:43:25 +0100

    Fix encoding in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf8") as f:
     long_description = f.read()
 
-with open("requirements.txt") as f:
+with open("requirements.txt", "r") as f:
     required_packages = f.read().splitlines()
+
 
 setup(
     name="comvis-dtu",
-    version="0.1",
+    version="0.2",
     description="A module gathering functionalities from the course 02504 Comuter Vision @ DTU (Spring 2023)",
     license="MIT",
     long_description=long_description,
